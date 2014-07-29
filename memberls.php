941eb8335fa3d1e41591c6b2ff2a930a6997bc67
pcchou <philipchou1009@gmail.com>
2014-07-29 17:03:10 +0800

    added memberls.php

---
+++ memberls.php
@@ -0,0 +1,21 @@
+<?php
+include 'config.php';
+
+
+$con = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if (!$con) {
+    trigger_error('Could not connect to MySQL: ' . mysqli_connect_error());
+}
+
+$sql = "SELECT BAHA_ID,HASHED_MAIL FROM list ORDER BY ID;";
+$result = mysqli_query($con, $sql);
+
+$var = array();
+
+while($obj = mysqli_fetch_object($result)) {
+    $var[] = $obj;
+}
+echo '{"list":'.json_encode($var).'}';
+
+?>
