aab425667fb031bc4b118670efd5a0e9eca55515
海豹 <007seadog@gmail.com>
2014-07-29 11:51:27 +0800

    Remove Ads
    I don't like ads

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-BA4B_Footer 
+<!--BA4B_Footer 
 <footer style="width: 728px;margin-left: auto;margin-right: auto">
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
@@ -8,6 +8,6 @@ BA4B_Footer
      data-ad-slot="6502030192"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+</script>-->
 <p>這個奇怪的程式是由<a href="http://about.me/pcchou">Pc Chou</a>、<a href="http://about.me/seadog007">海豹</a>、<a href="http://home.gamer.com.tw/happyjohn369">曙</a>、<a href="https://www.facebook.com/petercomm.hsu">Petercommand</a>、<a href="http://about.me/denny0223">Denny Huang</a>…等數不清的人一起協作，才能問世的。<br>前往<a href="https://github.com/ba4b">我們的GitHub專案</a>來了解更多！<br><br><br><br>聯絡我們：<a href="mailto:better.avatar.for.bahamut@gmail.com">better.avatar.for.bahamut@gmail.com</a></p>
       </footer>
