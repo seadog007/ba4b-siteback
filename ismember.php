6683c04645a2dfff0bda8c7a2d9bc89175162db2
pcchou <philipchou1009@gmail.com>
2014-07-29 17:53:38 +0800

    added ismember API

---
+++ ismember.php
@@ -0,0 +1,20 @@
+<?php
+include 'config.php';
+
+
+$con = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$name = $_GET["name"];
+
+if (!$con) {
+    trigger_error('Could not connect to MySQL: ' . mysqli_connect_error());
+}
+
+if (preg_match("/[a-zA-Z0-9]/",$name)) {
+    $sql = "SELECT EXISTS(SELECT BAHA_ID FROM list WHERE BAHA_ID='" . $name . "' LIMIT 1)";
+    $result = mysqli_query($con, $sql);
+    $data  = $result->fetch_array();
+    echo $data[0];
+} else {
+    trigger_error('Don\'t try SQL Injection!');
+}
+
