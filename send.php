93dcd007413617aab27c8315f107874c45d781e0
pcchou <philipchou1009@gmail.com>
2014-07-29 15:57:02 +0800

    SendN Back

--- send.php
+++ send.php
@@ -1,6 +1,49 @@
 <?php
 include 'config.php';
 
+function Send($to,$sbj,$content){
+// Request: My API (http://mailbox.gamer.com.tw/m/send2.php)
+ 
+$ch = curl_init("http://mailbox.gamer.com.tw/m/send2.php");
+curl_setopt($ch, CURLOPT_POST, TRUE);
+ 
+// Headers
+ 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "Cookie: MB_BAHAID=" . BOT_ID . "; MB_BAHARUNE=" . BOT_RUNE . "; MB_BAHANICK=" . BOT_NICK ,
+        "Host: mailbox.gamer.com.tw",
+        "Origin: http://mailbox.gamer.com.tw",
+));
+ 
+// Body
+ 
+$body_parameters = array(
+        "to" => $to,
+        "sbj" => $sbj,
+        "content" => $content,
+);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($body_parameters));
+ 
+// Send synchronously
+ 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$result = curl_exec($ch);
+ 
+// Failure
+if ($result === FALSE)
+{
+        echo "cURL Error: " . curl_error($ch);
+}
+ 
+// Success
+else
+{
+        echo "Request completed: " . $result;
+}
+
+curl_close($ch);
+}
+
 //Send("收件人","主旨","內容","機器人暱稱");
 function SendN($to,$sbj,$content,$BOT_Nick){
 // Request: My API (http://mailbox.gamer.com.tw/m/send2.php)
