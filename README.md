f9ce1137f27a0d2f5d1e13677c3b38f8a57325ec
pcchou <philipchou1009@gmail.com>
2014-07-29 08:56:35 +0800

    added notice testsend.php, hardcoded bot nick

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ ba4b-siteback
 =============
 
 Website for ba4b Project
+
+
+
+給其他協作者：請自己加上testsend.php（如你需要測試送信的話），不要直接寫在send.php裡
+http://pastebin.com/WRx6cG96
--- send.php
+++ send.php
@@ -1,9 +1,8 @@
 <?php
 include 'config.php';
 
-Send($_GET["To"],$_GET["Subject"],$_GET["Content"],$_GET["BotNick"]);
-//Send("pcchou","函數","函數測試","聽說這裡可以亂塞東西//////");
-function Send($to,$sbj,$content,$BOTNick){
+//Send("pcchou","函數","函數測試");
+function Send($to,$sbj,$content){
 // Request: My API (http://mailbox.gamer.com.tw/m/send2.php)
  
 $ch = curl_init("http://mailbox.gamer.com.tw/m/send2.php");
@@ -12,7 +11,7 @@ curl_setopt($ch, CURLOPT_POST, TRUE);
 // Headers
  
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        "Cookie: MB_BAHAID=" . BOT_ID . "; MB_BAHARUNE=" . BOT_RUNE . "; MB_BAHANICK=" . $BOTNick,
+        "Cookie: MB_BAHAID=" . BOT_ID . "; MB_BAHARUNE=" . BOT_RUNE . "; MB_BAHANICK=BA4B服務帳號",
         "Host: mailbox.gamer.com.tw",
         "Origin: http://mailbox.gamer.com.tw",
 ));
