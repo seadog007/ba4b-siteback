c4b6255d2c0a06bb4a95fad0bd51ac7114c32593
seadog007 <007seadog@gmail.com>
2014-07-29 15:50:16 +0800

    加入過濾條件

--- sendverify.php
+++ sendverify.php
@@ -1,8 +1,12 @@
 <?php
 include "send.php";
 $Name = $_POST["Name"];
-$Time = time();
-$Exp = time() + (60 * 60 * 2);
-$Hash = md5(md5($Time . $Name));
-echo $Name . "<br>" . $Time . "<br>" . $Exp . "<br>" . $Hash
+if(preg_match($Name, "[^a-zA-Z0-9]")||strlen($Name)>=13){
+	echo "Error"
+}else{
+	$Time = time();
+	$Exp = time() + (60 * 60 * 2);
+	$Hash = md5(md5($Time . $Name));
+	echo $Name . "<br>" . $Time . "<br>" . $Exp . "<br>" . $Hash
+}
 ?>
