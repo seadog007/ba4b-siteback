03117c2bb87f7d1b124f1cdb4bceb040e8f42a64
pcchou <philipchou1009@gmail.com>
2014-07-29 15:19:24 +0800

    deleted not-useful <br>s

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 <a href="https://github.com/ba4b"><img style="position: absolute; top: 150; right: 0; border: 0; width: 149px; height: 149px;" src="http://aral.github.com/fork-me-on-github-retina-ribbons/right-green@2x.png" alt="Fork me on GitHub"></a>
         <h1>巴哈頭像大改造<br>　　Better Avatar for Bahamut</h1>
         <p><h3 style="text-align:right;">一個連接Gravatar跟巴哈帳號的橋樑服務（瀏覽器插件）。</h3></p>
-        <p><br>這是由一些非常閒的人所發起的計劃，<br>原因就只是因為巴哈的勇造太醜了…XD<br><br></p>
+        <p><br>這是由一些非常閒的人所發起的計劃，<br>原因就只是因為巴哈的勇造太醜了…XD</p>
         <p><a href="#" class="btn btn-success btn-lg" data-toggle="modal" data-target="#basicModal">立即使用！ &raquo;</a></p>
       </div>
     </div>
