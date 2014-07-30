2ea83d2ee490fc4a70fd77d1445967c892c89fa6
pcchou <philipchou1009@gmail.com>
2014-07-30 17:25:20 +0800

    Added testsendmail.php. (sendmail undone)

--- sendmail.php
+++ sendmail.php
@@ -1,3 +1,32 @@
 <?php
+require("./phpmailer/class.phpmailer.php");
+include 'config.php';
 
+function SendMail($to,$sbj,$content){
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->SMTPAuth = true;
+    
+    $mail->Username = SMTP_USER;
+    $mail->Password = SMTP_PASS;
+    $mail->Host = SMTP_HOST;      
+    $mail->Port = SMTP_PORT;  
+    $mail->From = SMTP_EMAIL;
+    $mail->FromName = SMTP_NICK;    
+    
+    $mail->IsHTML(true);
+    
+    $mail->AddAddress($to,'親愛der用戶');
+    $mail->AddReplyTo(SMTP_EMAIL,"Squall.f");
+    
+    $mail->Subject = $sbj;
+    $mail->Body = $content;
+    
+    if(!$mail->Send()){
+        echo "哭哭" . $mail->ErrorInfo;
+        return 0;
+    }else{
+        return 1;
+    }
+}
 ?>
