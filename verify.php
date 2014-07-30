fa985ba652039aa5c1d1d55d7d988370c32248ba
seadog007 <007seadog@gmail.com>
2014-07-30 15:39:49 +0800

    Use isset()

--- verify.php
+++ verify.php
@@ -1,13 +1,17 @@
 <?php
-if($_GET["mode"]=="baha"){
-echo $_GET["mode"] . "/n";
-echo $_GET["id"] . "/n";
-echo $_GET["hash"] . "/n";
-}else if($_GET["mode"]=="email"){
-echo $_GET["mode"] . "/n";
-echo $_GET["id"] . "/n";
-echo $_GET["email"] . "/n";
-echo $_GET["hash"] . "/n";
+$mode = isset($_GET["mode"]) ? $_GET["mode"] : "" ;
+$id = isset($_GET["id"]) ? $_GET["id"] : "" ;
+$email = isset($_GET["email"]) ? $_GET["email"] : "" ;
+$hash = isset($_GET["hash"]) ? $_GET["hash"] : "" ;
+if($mode=="baha"){
+echo $mode . "/n";
+echo $id . "/n";
+echo $hash . "/n";
+}else if($mode=="email"){
+echo $mode . "/n";
+echo $id . "/n";
+echo $email . "/n";
+echo $hash . "/n";
 }else{
 	die("0");
 }
