c9c0590f3d088a4b4a531cbf8a4a066238959ec0
海豹 <007seadog@gmail.com>
2014-07-30 21:12:19 +0800

    說好的字數限制呢

--- sendverify.php
+++ sendverify.php
@@ -4,7 +4,7 @@ include "includes/send.php";
 
 $Name = $_POST["Name"];
 
-if(preg_match("/[a-zA-Z0-9]/",$Name)){
+if(preg_match("/[a-zA-Z0-9]{1,12}/",$Name)){
 $Time = time();
 $Exp = time() + (60 * 60 * 2);
 $Time = date("Y-m-d H:i:s",$Time); 
