fc8c53da6dd7ee81408b1d77363bc5e100d45847
seadog007 <007seadog@gmail.com>
2014-07-30 15:19:40 +0800

    test

--- verify.php
+++ verify.php
@@ -1,3 +1,14 @@
 <?php
-
+if($_GET["mode"]=="baha"){
+echo $_GET["mode"] . "/n";
+echo $_GET["id"] . "/n";
+echo $_GET["hash"] . "/n";
+}else if($_GET["mode"]=="email"){
+echo $_GET["mode"] . "/n";
+echo $_GET["id"] . "/n";
+echo $_GET["email"] . "/n";
+echo $_GET["hash"] . "/n";
+}else{
+	die("0");
+}
 ?>
