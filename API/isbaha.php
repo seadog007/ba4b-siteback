6a196340fedcc7807d04385ea2fa9aa9da4f7540
seadog007 <007seadog@gmail.com>
2014-07-31 17:16:32 +0800

    Fix bug

--- API/isbaha.php
+++ API/isbaha.php
@@ -4,7 +4,7 @@ $ch = curl_init("http://home.gamer.com.tw/homeindex.php?owner=".$user);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "--__X_PAW_BOUNDARY__--\r\n");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $result = curl_exec($ch);
-
+curl_close($ch);
 if ($result === FALSE){
 }else{
 	if(@preg_match("/巴哈姆特系統訊息/",$result)||$user==""){
@@ -14,7 +14,5 @@ if ($result === FALSE){
 	}
 }
 
-curl_close($ch);
 }
-echo isbahamember(isset($_GET["name"]) ? $_GET["name"] : "");
 ?>
