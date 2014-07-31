fea682986e0bb1b9055da883cd196709fba2d5d7
pcchou <philipchou1009@gmail.com>
2014-07-31 22:11:27 +0800

    Fix issue #27 - 自己被assign的issue自己修

--- setmail.php
+++ setmail.php
@@ -2,7 +2,9 @@
 function updatamail($id,$email,$ip){
 	$hashmail = md5(strtolower(trim($email)));
 	$con = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or trigger_error('Could not connect to MySQL: ' . mysqli_connect_error());
-	$sql = "UPDATE  `list` SET  `BAHA_NAME` = '" . getbahaname($id) . "',`EMAIL` =  '" . $email . "',`HASHED_MAIL` =  '" . $hashmail . "',`MODIFY_IP` =  '" . $ip . "' WHERE `BAHA_ID`='" . $id . "'";
+	$Time = time();
+	$Time = date("Y-m-d H:i:s",$Time); 
+	$sql = "UPDATE  `list` SET  `BAHA_NAME` = '" . getbahaname($id) . "',`EMAIL` =  '" . $email . "',`HASHED_MAIL` =  '" . $hashmail . "',`MODIFY_IP` =  '" . $ip . "',`MODIFY_IP` = '" . $Time . " WHERE `BAHA_ID`='" . $id . "'";
 	@mysqli_query($con, $sql);
 	return true;
 }
