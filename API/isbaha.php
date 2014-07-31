c1f25d12b2b93a681eb8ecfa9b74c480d99c4f85
seadog007 <007seadog@gmail.com>
2014-07-31 17:26:18 +0800

    Add new function

--- API/isbaha.php
+++ API/isbaha.php
@@ -1,5 +1,5 @@
 <?php
-function isbahamember($user){
+function bahamembername($user){
 $ch = curl_init("http://home.gamer.com.tw/homeindex.php?owner=".$user);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "--__X_PAW_BOUNDARY__--\r\n");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
@@ -7,12 +7,11 @@ $result = curl_exec($ch);
 curl_close($ch);
 if ($result === FALSE){
 }else{
-	if(@preg_match("/<span class="TS2">.{1,}<\/span>/",$result,$matches)||$user==""){
-		echo $matches;
+	if(@preg_match("/巴哈姆特系統訊息/",$result)||$user==""){
+		return 0;
 	}else{
-		return "N/A";
+		return 1;
 	}
 }
-
 }
 ?>
