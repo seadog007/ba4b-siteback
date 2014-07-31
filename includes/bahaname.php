9186ae47a36b546a790f14f990977e35f3237fc6
seadog007 <007seadog@gmail.com>
2014-07-31 20:08:50 +0800

    更改變數名稱避免衝突

--- includes/bahaname.php
+++ includes/bahaname.php
@@ -1,16 +1,16 @@
 <?php
 function getbahamembername($user){
-	$chh = curl_init("http://home.gamer.com.tw/homeindex.php?owner=" . $user);
+  $chh = curl_init("http://home.gamer.com.tw/homeindex.php?owner=" . $user);
 	curl_setopt($chh, CURLOPT_POSTFIELDS, "--__X_PAW_BOUNDARY__--\r\n");
 	curl_setopt($chh, CURLOPT_RETURNTRANSFER, TRUE);
 	$res = curl_exec($chh);
-	curl_close($ch);
+	curl_close($chh);
 	if ($res === FALSE){
 	}else{
-  		if(preg_match_all('/<span class="TS2">.+<\/span>/',$res,$match)||$user==""){
+  		if(preg_match_all('/<span class="TS2">.+<\/span>/',$res,$match)){
     		preg_replace('/<span class="TS2">/','',$match[0][1]);
     		preg_replace('/<\/span>/','',$match[0][1]);
-    	return $match[0][1];
+    	  return $match[0][1];
 		}else{
 			return "N/A";
 		}
