01a980107f5914a7d0e15cf367c1e3020798b842
seadog007 <007seadog@gmail.com>
2014-08-01 00:37:48 +0800

    再當一次我的個人專屬FTP吧 ^^

--- dist/js/checkform.js
+++ dist/js/checkform.js
@@ -12,6 +12,8 @@ function checkform(){
 	var str = window.location.pathname;
 	var isbaha = httpGet(window.location.origin + str.replace("/use.php", "") + "/API/isbaha.php?name=" + document.getElementById("Name").value);
 	if(isbaha == "1"){
+        var btn = $('#verify');
+        btn.button('傳送中...');
       	return true;
 	}else{
 		flash('#Name',8,10,100);
@@ -29,8 +31,6 @@ function isEmail() {
 }
 
 function sub(){
-document.getElementById('verify').value="傳送中...";
-document.getElementById('verify').disabled="disabled";
 var name = document.getElementById('Name').value;
 $.ajax({
   type: 'POST',
@@ -46,8 +46,6 @@ $(".f2").stop(true,false).animate({right:'0px',opacity: 1});
 }
 
 function sub2(){
-document.getElementById('verify').value="傳送中...";
-document.getElementById('verify').disabled="disabled";
 var name = document.getElementById('name').value;
 var hash = document.getElementById('hash').value;
 var email = document.getElementById('email').value;
@@ -63,4 +61,12 @@ $.ajax({
 $(".f1").stop(true,false).animate({left:'-=500px',opacity: 0});
 $(".f2").stop(true,false).animate({right:'0px',opacity: 1});
 }
+
+$('#verify').click(function () {
+    var btn = $(this)
+    btn.button('傳送中...')
+    $.ajax(...).always(function () {
+      btn.button('reset')
+    });
+  });
 
