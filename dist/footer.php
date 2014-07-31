244d02745e7425a691ece90e5cc60ac86bc8426c
pcchou <philipchou1009@gmail.com>
2014-07-31 16:29:56 +0800

    added customfooter.php

--- dist/footer.php
+++ dist/footer.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
   <footer style="width: 728px;margin-left: auto;margin-right: auto">
 	<div class="row">
+	<?php file_exists(custom_footer.php) AND include "custom_footer.php"; ?>
     <p>這個奇怪的程式是由<a href="http://about.me/pcchou">Pc
     Chou</a>、<a href="http://about.me/seadog007">海豹</a>、<a href=
     "http://home.gamer.com.tw/happyjohn369">曙</a>、<a href=
