a826c2b6fa835fbf82d5e1bb3ed8489db6efee80
pcchou <philipchou1009@gmail.com>
2014-07-31 16:27:01 +0800

    I WANT BUTTONS

--- index.php
+++ index.php
@@ -87,10 +87,14 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+        <h4 class="modal-title" id="myModalLabel">軟體授權</h4>
       </div>
       <div class="modal-body">
-        ...
+        <p>本程式所有部份皆採：<br>
+              <a href="http://opensource.org/licenses/MIT">The MIT License</a><br>
+              （MIT 許可證）<br>
+              授權，你可以依照授權條款來對本軟體修改、編輯、轉載…等<br>
+              不過，我們更希望你可以前往<a href="https://github.com/ba4b">我們的GitHub專案</a><br>來進行改良、或是提出建議！</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -104,27 +108,19 @@
     <div class="container">
       <div class="row">
         <div class="col-md-4">
-            <center><h2>效果介紹</h2></center>
-            <button class="btn btn-default" data-toggle="modal" data-target="#D1">
-            看更多 »
-            </button>
+            <center><button class="btn btn-default btn-lg" data-toggle="modal" data-target="#D1">
+            <h3>效果介紹</h3>
+            </button></center>
         </div>
         <div class="col-md-4">
-            <center><h2>如何使用</h2></center>
-            <button class="btn btn-default" data-toggle="modal" data-target="#D2">
-            看更多 »
-            </button>
+            <center><button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#D1">
+            <h3>如何使用</h3>
+            </button></center>
         </div>
         <div class="col-md-4">
-            <center><h2>軟體授權</h2></center>
-            <p><br>本程式所有部份皆採：<br>
-              <a href="http://opensource.org/licenses/MIT">The MIT License</a><br>
-              （MIT 許可證）<br>
-              授權，你可以依照授權條款來對本軟體修改、編輯、轉載…等<br>
-              不過，我們更希望你可以前往<a href="https://github.com/ba4b">我們的GitHub專案</a><br>來進行改良、或是提出建議！</p>
-              <button class="btn btn-default" data-toggle="modal" data-target="#D3">
-              看更多 »
-              </button>
+            <center><button class="btn btn-info btn-lg" data-toggle="modal" data-target="#D1">
+            <h3>軟體授權</h3>
+            </button></center>
         </div>
         
       </div>
