fb8d1b2c0c5678991c6ff8bb382ebc5082f4cc6b
seadog007 <007seadog@gmail.com>
2014-07-31 19:56:41 +0800

    移除echo

--- includes/bahaname.php
+++ includes/bahaname.php
@@ -8,7 +8,6 @@ curl_close($ch);
 if ($result === FALSE){
 }else{
   if(preg_match_all('/<span class="TS2">.+<\/span>/',$result,$matches)||$user==""){
-  	echo $matches[0][1];
     preg_replace('/<span class="TS2">/','',$matches[0][1]);
     preg_replace('/<\/span>/','',$matches[0][1]);
     	return $matches[0][1];
