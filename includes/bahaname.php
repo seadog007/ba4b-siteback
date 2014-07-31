c4701a3b185b577b7e323ca35a89e2980b94f69e
seadog007 <007seadog@gmail.com>
2014-07-31 19:58:17 +0800

    Fix bug

--- includes/bahaname.php
+++ includes/bahaname.php
@@ -1,19 +1,18 @@
 <?php
 function getbahamembername($user){
-$ch = curl_init("http://home.gamer.com.tw/homeindex.php?owner=".$user);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "--__X_PAW_BOUNDARY__--\r\n");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-$result = curl_exec($ch);
-curl_close($ch);
-if ($result === FALSE){
-}else{
-  if(preg_match_all('/<span class="TS2">.+<\/span>/',$result,$matches)||$user==""){
-    preg_replace('/<span class="TS2">/','',$matches[0][1]);
-    preg_replace('/<\/span>/','',$matches[0][1]);
+	$ch = curl_init("http://home.gamer.com.tw/homeindex.php?owner=".$user);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, "--__X_PAW_BOUNDARY__--\r\n");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	$result = curl_exec($ch);
+	curl_close($ch);
+	if ($result === FALSE){
+	}else{
+  		if(preg_match_all('/<span class="TS2">.+<\/span>/',$result,$matches)||$user==""){
+    	preg_replace('/<span class="TS2">/','',$matches[0][1]);
+    	preg_replace('/<\/span>/','',$matches[0][1]);
     	return $matches[0][1];
 	}else{
 		return "N/A";
 	}
-}
 }
 ?>
