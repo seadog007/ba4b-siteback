4769024b6113579e23190082b6eea6277e7c75ea
seadog007 <007seadog@gmail.com>
2014-07-31 18:09:36 +0800

    Add new function

--- API/bahaname.php
+++ API/bahaname.php
@@ -1,6 +1,5 @@
 <?php
-bahamembername($_GET["user"]);
-function bahamembername($user){
+function getbahamembername($user){
 $ch = curl_init("http://home.gamer.com.tw/homeindex.php?owner=".$user);
 curl_setopt($ch, CURLOPT_POSTFIELDS, "--__X_PAW_BOUNDARY__--\r\n");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
@@ -8,8 +7,10 @@ $result = curl_exec($ch);
 curl_close($ch);
 if ($result === FALSE){
 }else{
-	if(preg_match('/(<li>...<span class="TS2">.{1,}<\/span><\/li>\n){2}/m',$result,$matches)||$user==""){
-		echo $matches[0];
+  if(preg_match_all('/<span class="TS2">.+<\/span>/',$result,$matches)||$user==""){
+  	echo $matches[0][1];
+    preg_replace('/<span class="TS2">/','',$matches[0][1]);
+    preg_replace('/<\/span>/','',$matches[0][1]);
 	}else{
 		return "N/A";
 	}
