6cbdc050c986f5ab1eacd64221fbdc7198367efd
seadog007 <007seadog@gmail.com>
2014-07-31 17:27:53 +0800

    Add new function

---
+++ API/bahaname.php
@@ -0,0 +1,17 @@
+<?php
+function isbahamember($user){
+$ch = curl_init("http://home.gamer.com.tw/homeindex.php?owner=".$user);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "--__X_PAW_BOUNDARY__--\r\n");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$result = curl_exec($ch);
+curl_close($ch);
+if ($result === FALSE){
+}else{
+	if(@preg_match("/<span class="TS2">.{1,12}<\/span>/",$result,$matches)||$user==""){
+		echo $matches;
+	}else{
+		return "N/A";
+	}
+}
+}
+?>
