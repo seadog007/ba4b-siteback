d21e812c595c9f95633b1465e9e6f0525722494e
seadog007 <007seadog@gmail.com>
2014-07-31 20:30:29 +0800

    重建bahaname(2)

---
+++ includes/bahaname.php
@@ -0,0 +1,20 @@
+<?php
+echo "";
+function getbahaname($userid){
+  	$chh = curl_init("http://home.gamer.com.tw/homeindex.php?owner=" . $userid);
+	curl_setopt($chh, CURLOPT_RETURNTRANSFER, TRUE);
+	$res = curl_exec($chh);
+	if($res === FALSE){
+
+	}else{
+  		if(preg_match_all('/<span class="TS2">.+<\/span>/',$res,$match)){
+    		preg_replace('/<span class="TS2">/','',$match[0][1]);
+    		preg_replace('/<\/span>/','',$match[0][1]);
+    	  	return $match[0][1];
+		}else{
+			return "N/A";
+		}
+	}
+	curl_close($chh);
+}
+?>
