526d9755c7d2327f51d9f168ec7b087a83cd8320
pcchou <philipchou1009@gmail.com>
2014-07-31 09:34:02 +0800

    Added Ads ^_^

--- dist/footer.php
+++ dist/footer.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
-  <footer style=
-  "width: 728px;margin-left: auto;margin-right: auto">
-    <!--        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <footer style="width: 728px;margin-left: auto;margin-right: auto">
+	<div class="row">
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-1835688554454248"
-     data-ad-slot="6502030192"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>-->
-
+	<ins class="adsbygoogle"
+	     style="display:inline-block;width:728px;height:90px"
+	     data-ad-client="ca-pub-1835688554454248"
+	     data-ad-slot="6502030192"></ins>
+	<script>
+	(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>
+	</div>
+	<div class="row">
     <p>這個奇怪的程式是由<a href="http://about.me/pcchou">Pc
     Chou</a>、<a href="http://about.me/seadog007">海豹</a>、<a href=
     "http://home.gamer.com.tw/happyjohn369">曙</a>、<a href=
     "https://www.facebook.com/petercomm.hsu">Petercommand</a>、<a href="http://about.me/denny0223">Denny
     Huang</a>、<a href="home.gamer.com.tw/homeindex.php?owner=mmis1000">殘風</a>…等數不清的人一起協作，才能問世的。<br>
     前往<a href="https://github.com/ba4b">我們的GitHub專案</a>來了解更多！<br>
-    <br>
-    <br>
-    <br>
-    聯絡我們：<a href=
-    "mailto:better.avatar.for.bahamut@gmail.com">better.avatar.for.bahamut@gmail.com</a></p>
+    這個頁面上的廣告將會被用於改善本服務。
+    <br><br>
+    聯絡我們：<a href="mailto:better.avatar.for.bahamut@gmail.com">better.avatar.for.bahamut@gmail.com</a></p>
+    </div>
   </footer>
