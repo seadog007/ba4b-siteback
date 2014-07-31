676feac86c667181fb4eaecc9d200e7130bee9f0
seadog007 <007seadog@gmail.com>
2014-07-31 10:14:22 +0800

    Add offset

--- use.php
+++ use.php
@@ -31,7 +31,7 @@
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <div class="f1 col-md-4" align="center" valign="center">
+        <div class="f1 col-md-4 .col-md-offset-4" align="center" valign="center">
           <p><br>驗證您的巴哈帳號</p>
           <br>
           <form class="f1" action="sendverify.php" method="POST" onSubmit="return checkform()">
