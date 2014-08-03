3d75ea16766992bcb1cdf57d34c26d0b4506f877
pcchou <philipchou1009@gmail.com>
2014-08-03 07:57:15 +0000

    Added 揪's Contribution

--- dist/footer.php
+++ dist/footer.php
@@ -6,7 +6,7 @@
     <p>這個奇怪的程式是由<a href="http://about.me/seadog007">海豹</a>、<a href="http://about.me/pcchou">Pc
     Chou</a>、<a href="http://home.gamer.com.tw/homeindex.php?owner=mmis1000">殘風</a>、<a href=
     "http://home.gamer.com.tw/happyjohn369">曙</a>、<a href=
-    "https://www.facebook.com/petercomm.hsu">Petercommand</a>、<a href="http://about.me/denny0223">Denny
+    "http://home.gamer.com.tw/homeindex.php?owner=kyle23172982">揪凱</a>、<a href="http://about.me/denny0223">Denny
     Huang</a>…等數不清的人一起協作，才能問世的。<br>
     前往<a href="https://github.com/ba4b">我們的GitHub專案</a>來了解更多！<br>
     本程式所有部份皆採：<a href="http://opensource.org/licenses/MIT">The MIT License</a>（MIT 許可證）授權，你可以依照授權條款來對本軟體修改、編輯、轉載…等。（文件部份採<a href="https://creativecommons.org/licenses/by-sa/3.0/tw/">CC BY-SA 3.0</a>）<br>
