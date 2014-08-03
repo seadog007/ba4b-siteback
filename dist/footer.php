708c025dfabf4080ecfbdcc408f640282470aa2c
pcchou <philipchou1009@gmail.com>
2014-08-03 02:56:38 +0000

    Documentations use CC BY-SA 3.0

--- dist/footer.php
+++ dist/footer.php
@@ -9,7 +9,7 @@
     "https://www.facebook.com/petercomm.hsu">Petercommand</a>、<a href="http://about.me/denny0223">Denny
     Huang</a>…等數不清的人一起協作，才能問世的。<br>
     前往<a href="https://github.com/ba4b">我們的GitHub專案</a>來了解更多！<br>
-    本程式所有部份皆採：<a href="http://opensource.org/licenses/MIT">The MIT License</a>（MIT 許可證）授權，你可以依照授權條款來對本軟體修改、編輯、轉載…等。<br>
+    本程式所有部份皆採：<a href="http://opensource.org/licenses/MIT">The MIT License</a>（MIT 許可證）授權，你可以依照授權條款來對本軟體修改、編輯、轉載…等。（文件部份採<a href="https://creativecommons.org/licenses/by-sa/3.0/tw/">CC BY-SA 3.0</a>）<br>
     不過，我們更希望你可以前往<a href="https://github.com/ba4b">我們的GitHub專案</a>來進行改良、或是提出建議！<br>
     這個頁面上的廣告將會被用於改善本服務！<br>
     大家也可以到<a href="http://webchat.freenode.net/?channels=%23ysitd">Freenode IRC上的#ysitd</a>一起跟我們交流、切磋！<br>
