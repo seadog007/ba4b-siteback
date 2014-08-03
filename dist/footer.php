c7cf424c2d3cb473d6842a5f2bd7cf04e169ccd1
pcchou <philipchou1009@gmail.com>
2014-08-03 02:39:30 +0000

    Added notice for #ysitd

--- dist/footer.php
+++ dist/footer.php
@@ -11,7 +11,8 @@
     前往<a href="https://github.com/ba4b">我們的GitHub專案</a>來了解更多！<br>
     本程式所有部份皆採：<a href="http://opensource.org/licenses/MIT">The MIT License</a>（MIT 許可證）授權，你可以依照授權條款來對本軟體修改、編輯、轉載…等。<br>
     不過，我們更希望你可以前往<a href="https://github.com/ba4b">我們的GitHub專案</a>來進行改良、或是提出建議！<br>
-    這個頁面上的廣告將會被用於改善本服務！
+    這個頁面上的廣告將會被用於改善本服務！<br>
+    大家也可以到<a href="http://webchat.freenode.net/?channels=%23ysitd">Freenode IRC上的#ysitd</a>一起跟我們交流、切磋！<br>
     <br><br>
     聯絡我們：<a href="mailto:better.avatar.for.bahamut@gmail.com">better.avatar.for.bahamut@gmail.com</a></p>
     </div>
