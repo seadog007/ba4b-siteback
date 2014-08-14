0b43041ad55e1c5818bfffcd09ed19695de3a643
pcchou <philipchou1009@gmail.com>
2014-08-14 22:11:24 +0800

    Replaced addslashes with mysqli_real_escape_string

--- verify.php
+++ verify.php
@@ -37,7 +37,7 @@ function updatamail($id,$email,$ip)
     $con->query("SET NAMES utf8");
     $Time = time();
     $Time = date("Y-m-d H:i:s",$Time);
-    $sql = "UPDATE `list` SET  `BAHA_NAME` = '" . addslashes(getbahaname($id)) . "',`EMAIL` =  '" . addslashes($email) . "',`HASHED_MAIL` =  '" . addslashes($hashmail) . "',`MODIFY_IP` =  '" . addslashes($ip) . "',`MODIFY_TIME` = '" . addslashes($Time) . "' WHERE `BAHA_ID`='" . addslashes($id) . "'";
+    $sql = "UPDATE `list` SET  `BAHA_NAME` = '" . mysqli_real_escape_string($con,getbahaname($id)) . "',`EMAIL` =  '" . mysqli_real_escape_string($con,$email) . "',`HASHED_MAIL` =  '" . mysqli_real_escape_string($con,$hashmail) . "',`MODIFY_IP` =  '" . mysqli_real_escape_string($con,$ip) . "',`MODIFY_TIME` = '" . mysqli_real_escape_string($con,$Time) . "' WHERE `BAHA_ID`='" . mysqli_real_escape_string($con,$id) . "'";
     @mysqli_query($con, $sql);
     return true;
 }
@@ -59,20 +59,20 @@ if($mode=="baha"){
       <div class="container">
         <div class="f1 col-md-4 col-md-offset-4" align="center" valign="center">';
     if($id!=""&&$hash!=""){
-        $sql = "SELECT `BAHA_ID`,`verifycomplete`,`EXPIRE_TIME` FROM `verify` WHERE `BAHA_HASH`='" . addslashes($hash) . "' order by `ID` desc LIMIT 1";
+        $sql = "SELECT `BAHA_ID`,`verifycomplete`,`EXPIRE_TIME` FROM `verify` WHERE `BAHA_HASH`='" . mysqli_real_escape_string($con,$hash) . "' order by `ID` desc LIMIT 1";
         $result = @mysqli_query($con, $sql);
         $data = $result->fetch_array();
 
-        $sql = "SELECT `verifycomplete` FROM `verify` WHERE `BAHA_ID`='" . addslashes($id) . "' order by `ID` desc LIMIT 1";
+        $sql = "SELECT `verifycomplete` FROM `verify` WHERE `BAHA_ID`='" . mysqli_real_escape_string($con,$id) . "' order by `ID` desc LIMIT 1";
         $result = @mysqli_query($con, $sql);
         $data2 = $result->fetch_array();
 
         $left_time = strtotime($data[2]) - strtotime($Time);
         if($data[0]==$id&&$data[1]==0&&$left_time>=0){
-            $sql = "UPDATE `verify` SET  `verifycomplete` =  '1' WHERE  `BAHA_HASH`='" . addslashes($hash) . "' order by `ID` desc LIMIT 1";
+            $sql = "UPDATE `verify` SET  `verifycomplete` =  '1' WHERE  `BAHA_HASH`='" . mysqli_real_escape_string($con,$hash) . "' order by `ID` desc LIMIT 1";
             mysqli_query($con, $sql);
             if(firstlogin($id)){
-                $sql = "INSERT INTO `list` (`ID`, `BAHA_ID`, `BAHA_NAME`, `EMAIL`, `HASHED_MAIL`, `REGISTER_TIME`, `REGISTER_IP`, `MODIFY_TIME`, `MODIFY_IP`) VALUES (NULL, '" . addslashes($id) . "','" . addslashes(getbahaname($id)) . "', '', '', '" . addslashes($Time) . "', '" . addslashes($IP) . "', '0000-00-00 00:00:00', '0.0.0.0')";
+                $sql = "INSERT INTO `list` (`ID`, `BAHA_ID`, `BAHA_NAME`, `EMAIL`, `HASHED_MAIL`, `REGISTER_TIME`, `REGISTER_IP`, `MODIFY_TIME`, `MODIFY_IP`) VALUES (NULL, '" . mysqli_real_escape_string($con,$id) . "','" . mysqli_real_escape_string($con,getbahaname($id)) . "', '', '', '" . mysqli_real_escape_string($con,$Time) . "', '" . mysqli_real_escape_string($con,$IP) . "', '0000-00-00 00:00:00', '0.0.0.0')";
                 mysqli_query($con, $sql);
             }
             echo '<p><br><br>驗證巴哈帳號成功，請等5秒…</p><script>setTimeout("location.href=\'userman.php?name=' . $id . '&hash=' . $hash . '\'",' . $ref_time . ');</script>';
@@ -96,7 +96,7 @@ if($mode=="baha"){
         $data = $result->fetch_array();
         $left_time = strtotime($data[3]) - strtotime($Time);
         if($data[0]==$id&&$data[1]==$email&&$data[2]==0&&$left_time>=0){
-            $sql = "UPDATE `emailverify` SET  `verifycomplete` =  '1' WHERE  `EMAIL_HASH`='" . addslashes($hash) . "' order by `ID` desc LIMIT 1";
+            $sql = "UPDATE `emailverify` SET  `verifycomplete` =  '1' WHERE  `EMAIL_HASH`='" . mysqli_real_escape_string($con,$hash) . "' order by `ID` desc LIMIT 1";
             @mysqli_query($con, $sql);
             updatamail($id,$email,$IP);
             echo '<p><br><br>已經幫你綁定Email帳號與巴哈帳號，<br>即將跳轉回首頁</p><script>setTimeout("location.href=\'index.php\'",' . $ref_time . ');</script>';
@@ -112,7 +112,7 @@ if($mode=="baha"){
 function firstlogin($id)
 {
     $con = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or trigger_error('Could not connect to MySQL: ' . mysqli_connect_error());
-    $sql = "SELECT `ID` FROM `list` WHERE `BAHA_ID`='" . addslashes($id) . "'";
+    $sql = "SELECT `ID` FROM `list` WHERE `BAHA_ID`='" . mysqli_real_escape_string($con,$id) . "'";
     $result = @mysqli_query($con, $sql);
     $data = $result->fetch_array();
     if($data[0]==""){
